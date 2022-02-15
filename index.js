8ffaa48773f9e42781e4edde743bb246b72eb97d
Gap6769 <90163336+Gap6769@users.noreply.github.com>
2022-02-15 15:12:59 -0300

    general update

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const path = require('path');
 
 
 app.use('/assets', express.static(path.join(__dirname, './public/assets/')));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     console.log(__dirname + '/views/index.html');
     res.sendFile(path.join(__dirname,'/views/index.html'))});
 
-
+   
 
 app.listen(80), () => {
     console.log('Server is running on port 3000');

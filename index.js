286a2471134daa32b847f200c89c42eab5667e24
Gap6769 <90163336+Gap6769@users.noreply.github.com>
2022-02-16 08:38:35 -0300

    v1.0cv

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ app.use('/assets', express.static(path.join(__dirname, './public/assets/')));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    console.log(__dirname + '/views/index.html');
     res.sendFile(path.join(__dirname,'/views/index.html'))});
 
-   
+    app.get('/cv', (req, res) => {
+    
+        res.sendFile(path.join(__dirname,'/views/cv.html'))});
+    
 
 app.listen(80), () => {
     console.log('Server is running on port 3000');

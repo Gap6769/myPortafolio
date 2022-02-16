92a94749f8537ef7075b7fa5b155461b0041fee8
Gap6769 <90163336+Gap6769@users.noreply.github.com>
2022-02-16 18:35:42 -0300

    general update
    Css fixing, form working, /cv, etc

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -145,7 +145,7 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
+      typeSpeed: 180,
       backSpeed: 50,
       backDelay: 2000
     });
@@ -264,4 +264,6 @@
 })()
 
 //
+
+
 
